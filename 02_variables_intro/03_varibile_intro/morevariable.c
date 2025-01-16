b1e7b07bc7bd4ed7e06871ff14407bb18eb5057b
AudreyLinz <alinfors7396@floridapoly.edu>
2025-01-16 16:30:34 +0000

    IDK whats happening

---
+++ 02_variables_intro/03_varibile_intro/morevariable.c
@@ -0,0 +1,13 @@
+//practice problem
+
+#include <stdio.h>
+
+int main(){
+    int courseID = 2271;
+    int studentID=10234;
+    int subtract=studentID-courseID;
+
+    //prints words
+    printf("Name: John Doe\nCourse ID: %d\nStudent ID: %d\nStudent ID - Course ID is: %d\n", courseID, studentID, subtract);
+    return 0;
+}
---
+++ 02_variables_intro/variables_intro.c
@@ -0,0 +1,27 @@
+/*
+introduction to variables
+*/
+
+// Preprocessor directive
+//stdio - standard input output
+#include <stdio.h>
+
+//Create a main function
+
+int main(){
+    //Create a variable
+    int x = 10;
+    int y = 69;
+    printf("x time %d\n", x);
+    printf("y time %d\n", y);
+    //converse specifier %d, puts the x where the %d is
+
+    //sum time
+    int sum = x + y;
+    printf("%d+%d=%d\n", x, y, sum);
+   
+   
+    return 0;
+
+    
+}
